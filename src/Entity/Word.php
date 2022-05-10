b9e829a70dbbb3a01e9e1b2e5664e29ccb80572a
filipJaci <filip.jakovljevic@hotmail.com>
2022-05-10 18:32:37 +0200

    Adds Word entity and its related tests

---
+++ src/Entity/Word.php
@@ -0,0 +1,167 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\WordRepository;
+use Doctrine\ORM\Mapping as ORM;
+use phpDocumentor\Reflection\Types\Boolean;
+
+#[ORM\Entity(repositoryClass: WordRepository::class)]
+class Word
+{
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private $string;
+
+    private $stringArray;
+
+    #[ORM\Column(type: 'integer')]
+    private $score;
+
+    function __construct($string) {
+        // Set string.
+        $this->setString($string);
+        // Set score.
+        $this->setScore(0);
+    }
+
+    private function getString(): ?string
+    {
+        return $this->string;
+    }
+
+    private function setString(string $string): self
+    {
+        $this->string = $string;
+
+        return $this;
+    }
+
+    private function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    private function setScore(int $score): self
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    private function setStringArray(): void
+    {
+        // Make an array out of the existing string.
+        $this->stringArray = str_split($this->string);
+    }
+
+    private function increaseScore($newPoints)
+    {
+        // Add new points to the exsisting score.
+        $this->score+=$newPoints;
+    }
+
+    public function calculateScore()
+    {
+        // Word passes as an actual word.
+        if($this->validateWord())
+        {
+            // Score a Word based on unique characters.
+            $this->scoreWordBasedOnUniqueCharacters();
+            // Word is a palindrome.
+            if($this->checkPalindrome($this->string))
+            {
+                // Increase score by 3.
+                $this->increaseScore(3);
+            }
+            // Word is not a palindrome.
+            else
+            {
+                // Word is almost a palindrome.
+                if($this->checkAlmostAPalindrome())
+                {
+                    // Increase score by 2.
+                    $this->increaseScore(2);
+                }
+            }
+            // return score.
+            return $this->getScore();
+        }
+    }
+
+    private function validateWord(): bool
+    {
+        // Validate Word through API.
+        $validationResult = true;
+        // API validation passed.
+        if($validationResult)
+        {
+            // Make an array out of the string, used in later validation.
+            $this->setStringArray();
+            // Validation passed.
+            return true;
+
+        }
+        // Validation failed.
+        return false;
+    }
+
+    private function scoreWordBasedOnUniqueCharacters(): void
+    {
+        // Filter array in order to get rid of duplicate members.
+        $uniqueArrayKeys = array_unique($this->stringArray);
+        // Return count of the remaining array members.
+        $this->increaseScore(count($uniqueArrayKeys));
+    }
+
+    private function checkPalindrome($string): bool
+    {
+        // Reverse the string.
+        $reverseString = strrev($string);
+        // String matches the reversed string.
+        if($reverseString === $string)
+        {
+            // Word is an palindrome.
+            return true;
+        }
+        // Arrays don't match.
+        // Word isn't a palindrome.
+        return false;
+    }
+
+    public function checkAlmostAPalindrome()
+    {
+        // Reverse the string.
+        $reverseString = strrev($this->string);
+        // Itterate through the string.
+        for($i = 0; $i < strlen($this->string); $i++)
+        {
+            // Characters between the two strings don't match, at least one of the two causes a problem.
+            if($this->string[$i] !== $reverseString[$i])
+            {
+                // Remove problematic character from the first string.
+                $shortenedString = substr_replace($this->string, '', $i, 1);                
+                // Word is now a palindrome.
+                if($this->checkPalindrome($shortenedString))
+                {
+                    // Word is almost a palindrome.
+                    return true;
+                }
+                // Word is not a palindrome yet.
+                else
+                {
+                    // Remove problematic character from the reversed string.
+                    $shortenedString = substr_replace($reverseString, '', -$i, 1);
+                    // Word is now a palindrome.
+                    if($this->checkPalindrome($shortenedString))
+                    {
+                        // Word is almost a palindrome.
+                        return true;
+                    }
+                }
+                // Word is not a palindrome.
+                return false;
+            }
+        }
+    }
+}
---
+++ tests/WordTest.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+use App\Entity\Word;
+
+class WordTest extends TestCase
+{
+    // public function a_word_can_be_validated_test(){}
+
+    /** @test */
+    public function a_5_letter_word_with_5_unique_characters_scores_5_points(): void
+    {
+        // Set a 5 letter Word with unique characters.
+        $string = 'words';
+        // Create a new Word object.
+        $word = new Word($string);
+        // Run score method.
+        $score = $word->calculateScore();
+        // Score is 5.
+        $this->assertEquals(5, $score);
+    }
+
+    /** @test */
+    public function a_5_letter_word_with_4_unique_characters_scores_4_points(): void
+    {
+        // Set a 5 letter Word with 4 unique characters.
+        $string = 'hello';
+        // Create a new Word object.
+        $word = new Word($string);
+        // Run score method.
+        $score = $word->calculateScore();
+        // Score is 4.
+        $this->assertEquals(4, $score);
+    }
+
+    /** @test */
+    public function word_racecar_is_scored_as_a_palindrome()
+    {
+        // Set a 7 letter Word that's a palindrome.
+        $string = 'racecar';
+        // Create a new Word object.
+        $word = new Word($string);
+        // Run score method.
+        $score = $word->calculateScore();
+        // Score is 7, 4 for the unique letters and 3 for being a palindrome.
+        $this->assertEquals(7, $score);
+    }
+
+    /** @test */
+    public function word_engage_is_scored_as_alomost_a_palindrome()
+    {
+        // Set a 6 letter Word that's almost a palindrome.
+        $string = 'engage';
+        // Create a new Word object.
+        $word = new Word($string);
+        // Run score method.
+        $score = $word->calculateScore();
+        // Score is 6, 4 for the unique letters and 2 for being almost a palindrome.
+        $this->assertEquals(6, $score);
+    }
+}
