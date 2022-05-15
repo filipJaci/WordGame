c51f2e69198b9664dcb9da5e1045bdaf2978daaf
filipJaci <filip.jakovljevic@hotmail.com>
2022-05-15 10:55:52 +0200

    Adds Vue.js to the project

---
+++ assets/components/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    Welcome to the Jungle!
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "A list of words",
+      words: []
+    };
+  },
+  mounted() {
+
+  }
+};
+</script>
+
+<style>
+.center {
+  text-align: center;
+}
+</style>
---
+++ src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+
+        $words = ['sky', 'cloud', 'wood', 'rock', 'forest',
+            'mountain', 'breeze'];
+
+        return $this->render('home/index.html.twig', [
+            'words' => $words
+        ]);
+    }
+}
